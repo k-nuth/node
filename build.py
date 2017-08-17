46ae997fe83825ac2a195fbf059490e5b5270641
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-17 13:49:38 -0300

    conan improvements

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="testing",
                                  remotes="https://api.bintray.com/conan/bitprim/bitprim")
     builder.add_common_builds(shared_option_name="bitprim-node:shared")
-    builder.password = os.getenv("CONAN_PASSWORD")
+    # builder.password = os.getenv("CONAN_PASSWORD")
 
 
     filtered_builds = []
--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,7 @@
 from conans import ConanFile, CMake
-# import os
+
+def option_on_off(option):
+    return "ON" if option else "OFF"
 
 class BitprimNodeConan(ConanFile):
     name = "bitprim-node"
@@ -8,8 +10,27 @@ class BitprimNodeConan(ConanFile):
     url = "https://github.com/bitprim/bitprim-node"
     description = "Bitcoin full node"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+
+    # options = {"shared": [True, False]}
+    # default_options = "shared=False"
+
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "with_remote_blockchain": [True, False],
+               "with_remote_database": [True, False],
+               "with_tests": [True, False],
+               "with_console": [True, False],
+               "with_litecoin": [True, False],
+    }
+
+    default_options = "shared=False", \
+        "fPIC=True", \
+        "with_remote_blockchain=False", \
+        "with_remote_database=False", \
+        "with_tests=True", \
+        "with_console=True", \
+        "with_litecoin=False"
+
     generators = "cmake"
     exports_sources = "src/*", "CMakeLists.txt", "cmake/*", "bitprim-nodeConfig.cmake.in", "include/*", "test/*"
     package_files = "build/lbitprim-node.a"
@@ -21,6 +42,17 @@ class BitprimNodeConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        
+        cmake.definitions["USE_CONAN"] = "ON"
+        cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
+        cmake.definitions["ENABLE_SHARED"] = option_on_off(self.options.shared)
+        cmake.definitions["ENABLE_POSITION_INDEPENDENT_CODE"] = option_on_off(self.options.fPIC)
+        cmake.definitions["WITH_REMOTE_BLOCKCHAIN"] = option_on_off(self.options.with_remote_blockchain)
+        cmake.definitions["WITH_REMOTE_DATABASE"] = option_on_off(self.options.with_remote_database)
+        cmake.definitions["WITH_TESTS"] = option_on_off(self.options.with_tests)
+        cmake.definitions["WITH_CONSOLE"] = option_on_off(self.options.with_console)
+        cmake.definitions["WITH_LITECOIN"] = option_on_off(self.options.with_litecoin)
+
         cmake.configure(source_dir=self.conanfile_directory)
         cmake.build()
 
