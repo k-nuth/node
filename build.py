be053d1f8a376cef6709923a6b0bec46af91e233
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-18 09:14:53 -0300

    adding mingw

--- build.py
+++ build.py
@@ -4,9 +4,9 @@ import os
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="testing",
                                  remotes="https://api.bintray.com/conan/bitprim/bitprim")
+
     builder.add_common_builds(shared_option_name="bitprim-node:shared")
     # builder.password = os.getenv("CONAN_PASSWORD")
-
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
