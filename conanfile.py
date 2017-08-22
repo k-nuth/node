0c74a439fa6d6dba4bbd9e591c9c5b65793f69d2
Dario Ramos <dario.fenix@gmail.com>
2017-08-22 18:26:36 -0300

    DEV-32: Conan on/off switches

--- conanfile.py
+++ conanfile.py
@@ -63,6 +63,7 @@ class BitprimNodeConan(ConanFile):
         cmake = CMake(self)
         
         cmake.definitions["USE_CONAN"] = "ON"
+        cmake.definitions["NO_CONAN_AT_ALL"] = "OFF"
         cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
         cmake.definitions["ENABLE_SHARED"] = option_on_off(self.options.shared)
         cmake.definitions["ENABLE_POSITION_INDEPENDENT_CODE"] = option_on_off(self.options.fPIC)
