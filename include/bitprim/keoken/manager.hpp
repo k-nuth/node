cdc6d13a6a556bab5d5fa99472923d82ddcb73ce
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-09-03 12:37:59 -0300

    Keoken support for chain reorgs (#34)
    * Keoken support for chain reorgs
    
    * keoken reorg algorithm changes

--- include/bitprim/keoken/manager.hpp
+++ include/bitprim/keoken/manager.hpp
@@ -33,36 +33,6 @@
 namespace bitprim {
 namespace keoken {
 
-// using keoken_state_create_asset_t = void (*)
-//                 (char const* /*asset_name*/, 
-//                 keoken_amount_t /*asset_amount*/, 
-//                 payment_address_t /*owner*/, 
-//                 uint64_t /*size_t*/ /*block_height*/, 
-//                 hast_t /*txid*/);
-
-// using keoken_state_create_balance_entry_t = void (*)
-//                 (keoken_asset_id_t /*asset_id*/, 
-//                 keoken_amount_t /*asset_amount*/, 
-//                 payment_address_t /*source*/, 
-//                 payment_address_t /*target*/,  
-//                 uint64_t /*size_t*/ /*block_height*/, 
-//                 hast_t /*txid*/);
-
-// using keoken_state_asset_id_exists_t = void (*)
-//                 (keoken_asset_id_t /*id*/);
-
-// using keoken_state_get_balance_t = void (*)
-//                 (keoken_asset_id_t /*id*/, 
-//                 payment_address_t /*addr*/);
-
-// using keoken_state_get_assets_by_address_t = void (*)
-//                 (payment_address_t /*addr*/);
-
-// using keoken_state_get_assets_t = void (*)(void);
-
-// using keoken_state_get_all_asset_addresses_t = void (*)(void);
-
-
 template <typename State>
 class BCN_API base_manager {
     using payment_address = bc::wallet::payment_address;
@@ -71,16 +41,18 @@ public:
     using get_assets_list = typename State::get_assets_list;
     using get_all_asset_addresses_list = typename State::get_all_asset_addresses_list;
 
+    //class invariant: keoken_genesis_height > 0 
+
     base_manager(bc::blockchain::block_chain& chain, size_t keoken_genesis_height)
+    // precondition: keoken_genesis_height > 0
         : keoken_genesis_height_(keoken_genesis_height)
-        // , state_(asset_id_initial)
         , chain_(chain)
         , interpreter_(state_, chain_)
         , initialized_(false)
         , processed_height_(keoken_genesis_height - 1)
     {}
 
-    // non-copyable class
+    // non-copyable and non-movable class
     base_manager(base_manager const&) = delete;
     base_manager& operator=(base_manager const&) = delete;
 
@@ -152,7 +124,7 @@ private:
     
     // A typical reorganization consists of one incoming and zero outgoing blocks.
     bool handle_reorganized(bc::code ec, size_t fork_height, bc::block_const_ptr_list_const_ptr const& incoming, bc::block_const_ptr_list_const_ptr const& outgoing) {
-        if (ec == bc::error::service_stopped) { //stopped() || 
+        if (ec == bc::error::service_stopped) {
             return false;
         }
 
@@ -167,6 +139,11 @@ private:
         
         if (processed_height_ + 1 < fork_height) {
             initialize_from_blockchain(processed_height_ + 1, fork_height - 1);
+        }
+
+        if ( ! outgoing || ! outgoing->empty()) {
+            //Reorg
+            state_.rollback_to(fork_height);
         }
 
         for (auto const& block_ptr: *incoming) {
@@ -204,6 +181,8 @@ template <>
 class BCN_API manager<state_delegated> : public base_manager<state_delegated> {
 public:
     using set_initial_asset_id_func = typename state_delegated::set_initial_asset_id_func;
+    using reset_func = typename state_delegated::reset_func;
+    using rollback_to_func = typename state_delegated::rollback_to_func;
     using create_asset_func = typename state_delegated::create_asset_func;
     using create_balance_entry_func = typename state_delegated::create_balance_entry_func;
     using asset_id_exists_func = typename state_delegated::asset_id_exists_func;
@@ -215,6 +194,8 @@ public:
     using base_manager<state_delegated>::base_manager;
 
     void configure_state(set_initial_asset_id_func set_initial_asset_id
+                    , reset_func reset
+                    , rollback_to_func rollback_to
                     , create_asset_func create_asset
                     , create_balance_entry_func create_balance_entry
                     , asset_id_exists_func asset_id_exists
@@ -224,6 +205,8 @@ public:
                     , get_all_asset_addresses_func get_all_asset_addresses) {
 
         state().set_initial_asset_id = set_initial_asset_id;
+        state().reset = reset;
+        state().rollback_to = rollback_to;
         state().create_asset = create_asset;
         state().create_balance_entry = create_balance_entry;
         state().asset_id_exists = asset_id_exists;
