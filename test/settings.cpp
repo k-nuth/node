edd97248be674e4c9694d84724015fd1e427dc24
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-15 22:02:00 -0300

    Never use Boost.Test again settings

--- test/settings.cpp
+++ test/settings.cpp
@@ -2,46 +2,42 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 #include <kth/node.hpp>
 
 using namespace kth;
 
-BOOST_AUTO_TEST_SUITE(settings_tests)
+// Start Boost Suite: settings tests
 
 // constructors
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE(settings__construct__default_context__expected)
-{
+TEST_CASE("settings  construct  default context  expected", "[settings tests]") {
     node::settings configuration;
-    BOOST_REQUIRE_EQUAL(configuration.sync_peers, 0u);
-    BOOST_REQUIRE_EQUAL(configuration.sync_timeout_seconds, 5u);
-    BOOST_REQUIRE_EQUAL(configuration.refresh_transactions, true);
+    REQUIRE(configuration.sync_peers == 0u);
+    REQUIRE(configuration.sync_timeout_seconds == 5u);
+    REQUIRE(configuration.refresh_transactions == true);
 }
 
-BOOST_AUTO_TEST_CASE(settings__construct__none_context__expected)
-{
+TEST_CASE("settings  construct  none context  expected", "[settings tests]") {
     node::settings configuration(infrastructure::config::settings::none);
-    BOOST_REQUIRE_EQUAL(configuration.sync_peers, 0u);
-    BOOST_REQUIRE_EQUAL(configuration.sync_timeout_seconds, 5u);
-    BOOST_REQUIRE_EQUAL(configuration.refresh_transactions, true);
+    REQUIRE(configuration.sync_peers == 0u);
+    REQUIRE(configuration.sync_timeout_seconds == 5u);
+    REQUIRE(configuration.refresh_transactions == true);
 }
 
-BOOST_AUTO_TEST_CASE(settings__construct__mainnet_context__expected)
-{
+TEST_CASE("settings  construct  mainnet context  expected", "[settings tests]") {
     node::settings configuration(infrastructure::config::settings::mainnet);
-    BOOST_REQUIRE_EQUAL(configuration.sync_peers, 0u);
-    BOOST_REQUIRE_EQUAL(configuration.sync_timeout_seconds, 5u);
-    BOOST_REQUIRE_EQUAL(configuration.refresh_transactions, true);
+    REQUIRE(configuration.sync_peers == 0u);
+    REQUIRE(configuration.sync_timeout_seconds == 5u);
+    REQUIRE(configuration.refresh_transactions == true);
 }
 
-BOOST_AUTO_TEST_CASE(settings__construct__testnet_context__expected)
-{
+TEST_CASE("settings  construct  testnet context  expected", "[settings tests]") {
     node::settings configuration(infrastructure::config::settings::testnet);
-    BOOST_REQUIRE_EQUAL(configuration.sync_peers, 0u);
-    BOOST_REQUIRE_EQUAL(configuration.sync_timeout_seconds, 5u);
-    BOOST_REQUIRE_EQUAL(configuration.refresh_transactions, true);
+    REQUIRE(configuration.sync_peers == 0u);
+    REQUIRE(configuration.sync_timeout_seconds == 5u);
+    REQUIRE(configuration.refresh_transactions == true);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// End Boost Suite
