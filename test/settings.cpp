3691ee7f9c4f2fa6a954173b91c681af5cb09ee1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-19 22:35:00 -0300

    settings needed to be modernized

--- test/settings.cpp
+++ test/settings.cpp
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(settings__construct__default_context__expected)
 
 BOOST_AUTO_TEST_CASE(settings__construct__none_context__expected)
 {
-    node::settings configuration(config::settings::none);
+    node::settings configuration(infrastructure::config::settings::none);
     BOOST_REQUIRE_EQUAL(configuration.sync_peers, 0u);
     BOOST_REQUIRE_EQUAL(configuration.sync_timeout_seconds, 5u);
     BOOST_REQUIRE_EQUAL(configuration.refresh_transactions, true);
