e0f220dcfa32c961b950bf80b68a35791718e55b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-21 21:25:00 -0300

    settings needed to be normalized

--- test/settings.cpp
+++ test/settings.cpp
@@ -30,7 +30,7 @@ BOOST_AUTO_TEST_CASE(settings__construct__none_context__expected)
 
 BOOST_AUTO_TEST_CASE(settings__construct__mainnet_context__expected)
 {
-    node::settings configuration(config::settings::mainnet);
+    node::settings configuration(infrastructure::config::settings::mainnet);
     BOOST_REQUIRE_EQUAL(configuration.sync_peers, 0u);
     BOOST_REQUIRE_EQUAL(configuration.sync_timeout_seconds, 5u);
     BOOST_REQUIRE_EQUAL(configuration.refresh_transactions, true);
