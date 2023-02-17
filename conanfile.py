5fe20a15a416fbee28fda20027c365c97b9fb5da
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-02-17 16:16:47 +0100

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -85,7 +85,7 @@ class KnuthNodeConan(KnuthConanFile):
             self.requires("tabulate/1.0@")
 
         if self.options.tests:
-            self.requires("catch2/2.13.8")
+            self.requires("catch2/3.2.1")
 
     def validate(self):
         KnuthConanFile.validate(self)
