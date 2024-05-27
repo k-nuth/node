9932064a7596ddb9b1371dc46389f1429dd3cccc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-05-27 18:37:16 +0200

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -68,7 +68,7 @@ class KnuthNodeConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("blockchain/0.38.0", transitive_headers=True, transitive_libs=True)
+        self.requires("blockchain/0.39.0", transitive_headers=True, transitive_libs=True)
 
         if self.settings.os != "Emscripten":
             self.requires("network/0.42.0", transitive_headers=True, transitive_libs=True)
