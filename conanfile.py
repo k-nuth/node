9693ca763626125c96ca7e4a5aa658bea0fecf7b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-05-12 22:49:11 +0200

    release v0.56.0

--- conanfile.py
+++ conanfile.py
@@ -72,7 +72,7 @@ class KnuthNodeConan(KnuthConanFileV2):
         self.requires("blockchain/0.49.0", transitive_headers=True, transitive_libs=True)
 
         if self.settings.os != "Emscripten":
-            self.requires("network/0.52.0", transitive_headers=True, transitive_libs=True)
+            self.requires("network/0.53.0", transitive_headers=True, transitive_libs=True)
 
         if self.options.statistics:
             self.requires("tabulate/1.0@", transitive_headers=True, transitive_libs=True)
